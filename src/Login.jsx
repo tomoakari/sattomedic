22516f6b7c71d6182251f7f033c84420000e1cab
tomoakari <realize.arts@gmail.com>
2022-08-08 22:24:44 +0900

    ボタンなどのCSS確認

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,6 +6,8 @@ export const Login = () => {
   return (
     <div className="App">
       <h1>login!!</h1>
+      <div className='button'>ログイン</div>
+      <div className='subbutton'>新規登録</div>
     </div>
   )
 }
